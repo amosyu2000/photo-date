70831df872b71b1b4d3237071287b34694f21d5e
Amos Yu <amosyu2000@gmail.com>
2024-12-08 20:05:29 -0500

    Switch to using `click` and `pipenv` libraries

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # Photo Date
 
-Rename photos based on their date metadata. Generates a log after renaming all photos in a given directory.
+Photo Date command line tools.
+
+## Requirements
+
+[Python](https://www.python.org/downloads/) and [pipenv](https://pipenv.pypa.io/en/latest/installation.html) installed on your machine.
 
 ## Installation
 
 1. Download the repository.
 1. Extract the .zip.
-1. Run `pip install -r requirements.txt` in the root directory (where the `requirements.txt` is found).
+1. Run `pipenv install` in the root directory (where the `Pipfile` is found).
 1. Install [ffmpeg](https://www.gyan.dev/ffmpeg/builds/) onto your machine by following [this guide](https://www.geeksforgeeks.org/how-to-install-ffmpeg-on-windows/).
-1. Run `python . -d="YOUR DIRECTORY"`.
-1. Run `python . -h` for help.
+1. Run `pipenv shell` to start the virtual environment.
+1. Run `python . --help` inside the virtual environment shell to view the command line documentation.
+
+## Commands
+
+### `edit`
+Edit the Date Taken metadata of photos.
+```bash
+python . edit --files "YOUR FILE 1" "YOUR FILE 2"
+```
+
+### `rename`
+Rename photos based on their date metadata. Generates a log after renaming all photos in a given directory.
+```bash
+python . rename --directory "YOUR DIRECTORY"
+```
---
+++ commands/rename.py
@@ -0,0 +1,59 @@
+from classes.csv_logger import CsvLogger
+from classes.file_factory import FileFactory
+from classes.incremental_name_generator import IncrementalNameGenerator
+import click
+import glob
+import os
+
+
+@click.command()
+@click.option('--directory', '-d', required=True, type=click.STRING, help='The directory containing the photos to rename.')
+class Rename:
+    """Rename photos based on their date metadata. Generates a log after renaming all photos in a given directory."""
+
+    def __init__(self, directory):
+
+        print(directory)
+
+        # Get all files in given directory
+        files = [FileFactory(path).get_file() for path in glob.glob(f"{directory}/**/*", recursive=True) if not os.path.isdir(path)]
+
+        # Confirm that the user wants to rename the files
+        confirmation = input(f"Rename {len(files)} files in directory '{os.path.basename(directory)}'? This action cannot be undone. (Y/n) ")
+        if (confirmation != "Y"):
+            return
+
+        files.sort(key=lambda file: file.get_path(), reverse=True)
+        logger = CsvLogger()
+        name_generator = IncrementalNameGenerator(len(files))
+
+        while len(files) > 0:
+            # Gather files with the same name into a list
+            like_files = [files.pop()]
+            while len(files) > 0 and files[-1].get_name() == like_files[0].get_name():
+                like_files.append(files.pop())
+
+            # Search for a non-zero datetime from the list of like files
+            dt = None
+            for file in like_files:
+                file_dt = None
+                try:
+                    file_dt = file.get_datetime()
+                except NotImplementedError as e:
+                    print(e)
+                if file_dt is not None:
+                    dt = file_dt
+                    break
+
+            # Apply the same datetime name to all like files
+            new_name = self.get_datetime_name(dt) if dt is not None else name_generator.get_next_name()
+            for file in like_files:
+                logger.log((file.get_path_tail(), file.rename(new_name)))
+
+        logger.save(os.path.dirname(directory))
+
+    def get_datetime_name(self, dt):
+        """
+        Return the new name for a file given its datetime information.
+        """
+        return f"{dt.year}_{dt.month:02d}_{dt.day:02d}-{dt.hour:02d}_{dt.minute:02d}_{dt.second:02d}"
